20ee70a0492420cbe919bee820fc01e06ffe0c8c
hejunjiecao <hejunjie.cao@tum.de>
2025-06-06 18:09:54 +0800

    day00

---
+++ week00/day00/34-search-range.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int search_left_border(int* nums, int numsSize, int target)
+{
+    int left = 0;
+    int right = numsSize - 1;
+    int left_border = -1;
+    while (left <= right)
+    {
+        int middle = left + (right - left) / 2;
+        if(nums[middle] < target)
+            left = middle + 1;
+        if(nums[middle] > target)
+            right = middle - 1;
+        if(nums[middle] == target)
+        {
+            left_border = middle;
+            right = middle - 1;
+        }
+    }
+    return left_border;
+}
+
+int search_right_border(int* nums, int numsSize, int target)
+{
+    int left  = 0;
+    int right = numsSize - 1;
+    int right_border = -1;
+    while (left <= right)
+    {
+        int middle = left + (right - left) / 2;
+        if(nums[middle] < target)
+            left = middle + 1;
+        if(nums[middle] > target)
+            right = middle - 1;
+        if(nums[middle] == target)
+        {
+            right_border = middle;
+            left = middle + 1;
+        }
+    }
+    return right_border;
+}
+
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int left_border = search_left_border(nums, numsSize, target);
+    int right_border = search_right_border(nums, numsSize, target);
+    int* result = malloc(2 * sizeof(int));
+    result[0] = left_border;
+    result[1] = right_border;
+    *returnSize = 2;
+    return result;
+}
--- week00/day00/35-search-insert-position.c
+++ week00/day00/35-search-insert-position.c
@@ -1,15 +1,16 @@
-int search(int* nums, int numsSize, int target) {
+int searchInsert(int* nums, int numsSize, int target) {
     int left = 0;
     int right = numsSize - 1;
+    int middle;
     while(left <= right)
     {
-        int middle = (left + right) / 2;
-        if(nums[middle] > target)
-            right = middle - 1;
+        middle = left + (right - left) / 2;
         if(nums[middle] < target)
             left = middle + 1;
+        if(nums[middle] > target)
+            right = middle - 1;
         if(nums[middle] == target)
             return middle;
     }
-    return -1;
+    return left;
 }
